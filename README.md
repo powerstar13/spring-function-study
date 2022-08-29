78aaaa8b73ad4b90225b68715a0b8e64cc6eeb11
홍준성 <ghdwnstjd13@gmail.com>
2022-08-29 16:19:54 +0900

    Custom Validation
    1. `AssertTrue/False`와 같은 method 지정을 통해서 Custom Logic 적용 가능

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 스프링의 기능을 활용해보자
 
 1. Spring Boot Validation
+2. Spring Boot Custom Validation
+    - @AssertTrue 애노테이션을 사용하여 유효성 검사 정의
--- validation/src/main/java/spring/function/study/dto/User.java
+++ validation/src/main/java/spring/function/study/dto/User.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -18,6 +20,22 @@ public class User {
     private String email;
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
     private String phoneNumber;
+    @Size(min = 6, max = 6)
+    private String reqYearMonth; // yyyyMM
+
+    @AssertTrue(message = "날짜 양식에 맞지 않습니다. yyyyMM") // @AssertTrue: return이 true면 정상, false면 실패
+    public boolean isReqYearMonthValidation() { // WARN: 메서드명은 boolean return 타입에 맞추어 is*로 시작해야 한다.
+        System.out.println("===== isReqYearMonthValidation() =====");
+
+        try {
+            LocalDate localDate = LocalDate.parse(reqYearMonth + "01", DateTimeFormatter.ofPattern("yyyyMMdd")); // LocalDate의 구조에 맞추기 위해 임시 일자 추가
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
 
     @Override
     public String toString() {
@@ -26,6 +44,7 @@ public class User {
             ", age=" + age +
             ", email='" + email + '\'' +
             ", phoneNumber='" + phoneNumber + '\'' +
+            ", reqYearMonth='" + reqYearMonth + '\'' +
             '}';
     }
 }
